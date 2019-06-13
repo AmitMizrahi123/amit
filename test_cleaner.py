1471d29f691a7ddd7a6dc0f28399bae4b7114092
amit <47501611+AmitMizrahi123@users.noreply.github.com>
2019-06-13 16:43:06 +0300

    Delete test_cleaner.py

--- test_cleaner.py
+++
@@ -1,9 +0,0 @@
-from OOP_Office.cleaner import Cleaner
-
-
-class TestCleaner:
-    def test_init(self):
-        amit = Cleaner("amit")
-        assert type(amit.name) == str
-        assert len(amit.name) == 4
-        assert amit.work() != "amit is cleaning"
