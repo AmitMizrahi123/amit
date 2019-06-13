cffba7b254bd5186c5a9e8fd331831883f7b5e63
amit <47501611+AmitMizrahi123@users.noreply.github.com>
2019-06-13 16:42:20 +0300

    Delete employee.py

--- employee.py
+++
@@ -1,8 +0,0 @@
-from .document import Document
-from .person import Person
-
-
-class Employee(Person): 
-    def work(self, office):
-        for i in range(0, 10):
-            office.documents.append(Document(self.name))
