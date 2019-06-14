c6f9091eece017a08127d7d9534b38029e328267
amit <47501611+AmitMizrahi123@users.noreply.github.com>
2019-06-14 08:37:35 +0300

    loading screen sizes

--- selenium/buymeauto.py
+++ selenium/buymeauto.py
@@ -1,14 +1,24 @@
-from selenium import webdriver
 import time
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome("chromedriver.exe")
+delay = 5
+
+capa = DesiredCapabilities.CHROME
+capa["pageLoadStrategy"] = "none"
+
+driver = webdriver.Chrome(executable_path="chromedriver.exe", desired_capabilities=capa)
+wait = WebDriverWait(driver, delay)
 driver.get("https://buyme.co.il/")
 
 
 #  function that enter into login / registration
 def entry_button():
-    for entryButton in driver.find_elements_by_xpath(r'//*[@id="ember591"]/div/ul[1]/li[3]/a/span[2]'):
-        entryButton.click()
+    time.sleep(5)
+    driver.find_element_by_xpath('//*[@id="ember591"]/div/ul[1]/li[3]/a/span[2]').click()
 
 
 # function that create account
@@ -88,3 +98,16 @@ def sender_receiver():
     driver.find_element_by_xpath('//input[@type="email"]').send_keys("amitmizrahi231055@gmail.com")
     time.sleep(1)
     driver.find_element_by_xpath('//*[@id="ember1310"]/div[4]/div/div[3]/div/div[2]/button[2]').click()
+
+
+def loading_screen():
+    try:
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#app-loading-img > div > div.bounce1')))
+        driver.execute_script("window.stop();")
+        print(driver.find_element_by_xpath('//*[@id="app-loading-img"]/div').size)
+        print(driver.find_element_by_xpath('//*[@id="app-loading-img"]/div/div[1]').size)
+        driver.quit()
+    except ValueError as err:
+        print('the site is not good ' + str(err))
+    finally:
+        driver.quit()
